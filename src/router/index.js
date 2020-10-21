1711daf3df23b87793ed165790f84946fd52ce56
whiscovery <whiscovery@gmail.com>
2020-10-22 00:01:54 +0900

    init

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,13 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/question",
+    name: "Qustion",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "question" */ "../views/Question.vue")
   }
 ];
 
---
+++ src/views/Question.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about"><Question /></div>
+</template>
+<script>
+import Question from "@/components/Question";
+
+export default {
+  components: [Question]
+};
+</script>
